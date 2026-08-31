1ef55f9c9788de9a5be12848b056019ad7e24754
hyouka007web <hyouka6@atomicmail.io>
2026-08-31 11:03:40 +0200

    Fix GeckoView dependency to official major release 135.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,7 +43,7 @@ android {
 }
 
 dependencies {
-    implementation("org.mozilla.geckoview:geckoview-omni:135.0.20250130094713")
+    implementation("org.mozilla.geckoview:geckoview:135.0")
     implementation("androidx.core:core-ktx:1.16.0")
     implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("com.google.android.material:material:1.13.0")
