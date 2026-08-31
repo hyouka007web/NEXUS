e69007bf321040999766b8a27e4b18475f1cd0df
hyouka007web <hyouka6@atomicmail.io>
2026-08-31 11:12:10 +0200

    Switch GeckoView to beta release version tag

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,7 +43,7 @@ android {
 }
 
 dependencies {
-    implementation("org.mozilla.geckoview:geckoview-omni:130.0.20240829073237")
+    implementation("org.mozilla.geckoview:geckoview-beta:122.0b9")
     implementation("androidx.core:core-ktx:1.16.0")
     implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("com.google.android.material:material:1.13.0")
