3c26960815494410bb29df2ebe04062b19751db7
hyouka007web <hyouka6@atomicmail.io>
2026-09-01 17:10:18 +0200

    fix: update GeckoView dependency and repositories

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,13 +44,13 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    
+
     // GeckoView Release
-    implementation("org.mozilla.geckoview:geckoview:121.0.20240108152208")
+    implementation("org.mozilla.geckoview:geckoview:121.0.20231211174248")
 
     implementation("androidx.recyclerview:recyclerview:1.3.2")
     implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
-    
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
