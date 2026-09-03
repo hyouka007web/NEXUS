6409f1f78135ec47c0c9cc9bb71f0be22db2afc9
hyouka007web <hyouka6@atomicmail.io>
2026-09-03 09:17:49 +0200

    fix: use valid geckoview nightly artifact release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,7 +43,7 @@ android {
 }
 
 dependencies {
-    implementation("org.mozilla.geckoview:geckoview:121.0.20240108143644")
+    implementation("org.mozilla.geckoview:geckoview-nightly:130.0.20240715094209")
     implementation("androidx.core:core-ktx:1.16.0")
     implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("com.google.android.material:material:1.13.0")
