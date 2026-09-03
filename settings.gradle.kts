11abc44a14de6960e759f3b7f4edaac047709ad9
hyouka007web <hyouka6@atomicmail.io>
2026-09-03 09:02:03 +0200

    fix: add mozilla repository and update geckoview version

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,7 +3,11 @@ pluginManagement {
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories { google(); mavenCentral() }
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://maven.mozilla.org/maven2/") }
+    }
 }
 rootProject.name = "NEXUS"
 include(":app")
