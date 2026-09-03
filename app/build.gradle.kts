f879acd127f00a14aa59354d8e2906aeafb01b0e
hyouka007web <hyouka6@atomicmail.io>
2026-09-03 09:11:50 +0200

    fix: change geckoview artifact name to geckoview-nightly-omni

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,7 +43,7 @@ android {
 }
 
 dependencies {
-    implementation("org.mozilla.geckoview:geckoview-omni:121.0.20231204155522")
+    implementation("org.mozilla.geckoview:geckoview-nightly-omni:122.0.20231213093226")
     implementation("androidx.core:core-ktx:1.16.0")
     implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("com.google.android.material:material:1.13.0")
