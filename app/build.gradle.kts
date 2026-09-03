1b2e63f374e994a5f919cd6c3206da076eaa3d41
hyouka007web <hyouka6@atomicmail.io>
2026-09-03 09:15:02 +0200

    fix: use valid geckoview release artifact and version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,7 +43,7 @@ android {
 }
 
 dependencies {
-    implementation("org.mozilla.geckoview:geckoview-nightly-omni:122.0.20231213093226")
+    implementation("org.mozilla.geckoview:geckoview:121.0.20240108143644")
     implementation("androidx.core:core-ktx:1.16.0")
     implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("com.google.android.material:material:1.13.0")
